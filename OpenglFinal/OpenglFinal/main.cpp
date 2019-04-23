5f8837deb0515ae41c503ec697bf7202802e7dfb
Zoroiscrying <zoroiscrying@outlook.com>
2019-04-23 22:50:39 +0800

    EBO VAO VBO

--- OpenglFinal/OpenglFinal/main.cpp
+++ OpenglFinal/OpenglFinal/main.cpp
@@ -4,11 +4,19 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+//顶点
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	-0.5f, -0.5f, 0.0f, //左下角
+	 0.5f, -0.5f, 0.0f, //右下角
+	 0.0f,  0.5f, 0.0f, //上角
+	 0.8f,  0.8f, 0.0f, //右上角
 };
+//绘制顺序
+unsigned int indices[] = {
+	0,1,2,
+	3,2,1
+};
+
 
 const char* vertexShaderSource =
 "#version 330 core                                    \n"
@@ -23,7 +31,7 @@ const char* fragmentShaderSource =
 "out vec4 FragColor;                          \n"
 "void main()                                  \n"
 "{                                            \n"
-"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}\n"
+"   FragColor = vec4(1.0f, 0.2f, 0.2f, 1.0f);}\n"
 ;
 
 void ProcessInput(GLFWwindow *window) {
@@ -57,6 +65,9 @@ int main() {
 	}
 
 	glViewport(0, 0, 800, 600);
+	//oepngl
+	//glEnable(GL_CULL_FACE);
+	//glCullFace(GL_BACK);
 
 	//shader
 	unsigned int VAO;
@@ -67,6 +78,11 @@ int main() {
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	unsigned int vertexShader;
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
@@ -84,10 +100,10 @@ int main() {
 	glAttachShader(shaderProgram, fragmentShader);
 	glLinkProgram(shaderProgram);
 	
-	//glUseProgram(shaderProgram);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -100,7 +116,9 @@ int main() {
 
 		glBindVertexArray(VAO);
 		glUseProgram(shaderProgram);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
 
 		//检查并调用事件，交换缓冲
 		glfwSwapBuffers(window);
