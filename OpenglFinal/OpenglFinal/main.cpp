883c055efbc54a329e51b19b77ab9e4523e34ec2
Zoroiscrying <zoroiscrying@outlook.com>
2019-04-23 16:57:27 +0800

    First Triangle Shader

--- OpenglFinal/OpenglFinal/main.cpp
+++ OpenglFinal/OpenglFinal/main.cpp
@@ -4,6 +4,35 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+};
+
+const char* vertexShaderSource =
+"#version 330 core                                    \n"
+"layout (location = 0) in vec3 aPos;                  \n"
+"void main()                                          \n"
+"{                                                    \n"
+"    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);}\n"
+;
+
+const char* fragmentShaderSource =
+"#version 330 core                            \n"
+"out vec4 FragColor;                          \n"
+"void main()                                  \n"
+"{                                            \n"
+"   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}\n"
+;
+
+void ProcessInput(GLFWwindow *window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwWindowShouldClose(window);
+	}
+}
+
 int main() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -29,8 +58,51 @@ int main() {
 
 	glViewport(0, 0, 800, 600);
 
+	//shader
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	unsigned int vertexShader;
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+
+	unsigned int shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	
+	//glUseProgram(shaderProgram);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE,3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+
 	while (!glfwWindowShouldClose(window))
 	{
+		//按键
+		ProcessInput(window);
+
+		//渲染
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBindVertexArray(VAO);
+		glUseProgram(shaderProgram);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//检查并调用事件，交换缓冲
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
